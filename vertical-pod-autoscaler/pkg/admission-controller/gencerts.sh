ab3ed94a3de7f018f3886159c737a00fd1f96221
Yuchi Chen <yuchi518@gmail.com>
2022-05-25 14:38:01 +0800

    force file path for openssl

--- vertical-pod-autoscaler/pkg/admission-controller/gencerts.sh
+++ vertical-pod-autoscaler/pkg/admission-controller/gencerts.sh
@@ -39,9 +39,9 @@ subjectAltName = DNS:vpa-webhook.kube-system.svc
 EOF
 
 # Create a certificate authority
-openssl genrsa -out ${TMP_DIR}/caKey.pem 2048
+/opt/homebrew/Cellar/openssl@1.1/1.1.1o/bin/openssl genrsa -out ${TMP_DIR}/caKey.pem 2048
 set +o errexit
-openssl req -x509 -new -nodes -key ${TMP_DIR}/caKey.pem -days 100000 -out ${TMP_DIR}/caCert.pem -subj "/CN=${CN_BASE}_ca" -addext "subjectAltName = DNS:${CN_BASE}_ca"
+/opt/homebrew/Cellar/openssl@1.1/1.1.1o/bin/openssl req -x509 -new -nodes -key ${TMP_DIR}/caKey.pem -days 100000 -out ${TMP_DIR}/caCert.pem -subj "/CN=${CN_BASE}_ca" -addext "subjectAltName = DNS:${CN_BASE}_ca"
 if [[ $? -ne 0 ]]; then
   echo "ERROR: Failed to create CA certificate for self-signing. If the error is \"unknown option -addext\", update your openssl version or deploy VPA from the vpa-release-0.8 branch."
   exit 1
@@ -49,10 +49,10 @@ fi
 set -o errexit
 
 # Create a server certiticate
-openssl genrsa -out ${TMP_DIR}/serverKey.pem 2048
+/opt/homebrew/Cellar/openssl@1.1/1.1.1o/bin/openssl genrsa -out ${TMP_DIR}/serverKey.pem 2048
 # Note the CN is the DNS name of the service of the webhook.
-openssl req -new -key ${TMP_DIR}/serverKey.pem -out ${TMP_DIR}/server.csr -subj "/CN=vpa-webhook.kube-system.svc" -config ${TMP_DIR}/server.conf -addext "subjectAltName = DNS:vpa-webhook.kube-system.svc"
-openssl x509 -req -in ${TMP_DIR}/server.csr -CA ${TMP_DIR}/caCert.pem -CAkey ${TMP_DIR}/caKey.pem -CAcreateserial -out ${TMP_DIR}/serverCert.pem -days 100000 -extensions SAN -extensions v3_req -extfile ${TMP_DIR}/server.conf
+/opt/homebrew/Cellar/openssl@1.1/1.1.1o/bin/openssl req -new -key ${TMP_DIR}/serverKey.pem -out ${TMP_DIR}/server.csr -subj "/CN=vpa-webhook.kube-system.svc" -config ${TMP_DIR}/server.conf -addext "subjectAltName = DNS:vpa-webhook.kube-system.svc"
+/opt/homebrew/Cellar/openssl@1.1/1.1.1o/bin/openssl x509 -req -in ${TMP_DIR}/server.csr -CA ${TMP_DIR}/caCert.pem -CAkey ${TMP_DIR}/caKey.pem -CAcreateserial -out ${TMP_DIR}/serverCert.pem -days 100000 -extensions SAN -extensions v3_req -extfile ${TMP_DIR}/server.conf
 
 echo "Uploading certs to the cluster."
 kubectl create secret --namespace=kube-system generic vpa-tls-certs --from-file=${TMP_DIR}/caKey.pem --from-file=${TMP_DIR}/caCert.pem --from-file=${TMP_DIR}/serverKey.pem --from-file=${TMP_DIR}/serverCert.pem
